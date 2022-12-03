89cc7bba914f58caba1784f1984b1b37c6a2e536
Heba-Webdev23 <jtvonline@yahoo.com>
2022-12-04 02:54:14 +0300

    feat mobile navbar

--- program.js
+++ program.js
@@ -1,2 +1,12 @@
 
-
+const menuBtn = document.querySelector('.navbar__btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+  if(!menuOpen) {
+    menuBtn.setAttribute("aria-expanded", true)
+    menuOpen = true;
+  } else {
+    menuBtn.setAttribute("aria-expanded", false);
+    menuOpen = false;
+  }
+});
