088fa6c4a84db4e78fab415a5d5904ec4fe99d33
Heba-Webdev23 <jtvonline@yahoo.com>
2022-12-08 03:57:44 +0300

    feat mobile nav and new footer

--- program.js
+++ program.js
@@ -1,12 +1,22 @@
 
 const menuBtn = document.querySelector('.navbar__btn');
+const navList = document.querySelector('.navbar__list');
 let menuOpen = false;
 menuBtn.addEventListener('click', () => {
   if(!menuOpen) {
     menuBtn.setAttribute("aria-expanded", true)
+    navList.classList.toggle("open");
     menuOpen = true;
   } else {
     menuBtn.setAttribute("aria-expanded", false);
+    navList.classList.toggle("open");
     menuOpen = false;
   }
 });
+
+
+const setCurrentYear = document.querySelector(".year");
+let date = new Date();
+let currentYear = date.getFullYear();
+setCurrentYear.innerHTML = currentYear;
+
